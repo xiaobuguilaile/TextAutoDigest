1dde8d54bb267475d80ec5ed5018baaec8df62aa
howling0 <tianyi_liu6799@163.com>
2020-03-09 12:25:30 +0800

    Integrated the model and front end

--- abstra.py
+++ abstra.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-from process import fila
+from process import extract
+import json
+import os
+from models.SIF_model import GETSentence_Embedding
 app = Flask(__name__)
 
 
@@ -8,29 +11,35 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/send_message/', methods=['GET'])
+@app.route('/send_message/', methods=['POST'])
 def send_message():
-    global message_get
-    message_get = ""
-
-    message_get = request.args['message']
-    message_get = fila(message_get)
-    print("摘要：%s" % message_get)
-    return '收到'
-
-
-@app.route('/change_to_json/', methods=['GET'])
-def change_to_json():
-
-    global message_get
+    # global message_get
+    message_get = request.form['message']
+    # message_get = request.args['message']
+    abstract = extract(model, message_get)
+    # TODO Inhence the output via syntax
+    print("摘要：%s" % abstract)
     message_json = {
-        "message": message_get
+        "code": 200,
+        "message": abstract
     }
-
     return jsonify(message_json)
 
 
+# @app.route('/change_to_json/', methods=['GET'])
+# def change_to_json():
+
+#     global message_get
+#     message_json = {
+#         "message": message_get
+#     }
+
+#     return jsonify(message_json)
+
+
 if __name__ == '__main__':
+    path = os.path.abspath("./data/word_vectors_100")
+    model = GETSentence_Embedding(path)
     app.run()
 
 
